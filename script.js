b9cf82ba638286dfd98668a6a6159dde19885c3e
timm647 <timwars250@gmail.com>
2026-06-24 22:32:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -558,8 +558,8 @@ const tickets = [
       },
       {
         "type": "text",
-        "title": "Épreuve II — Les lettres de la nuit",
-        "q": "<p>Les lettres se sont mélangées avant de s'endormir.</p><p><strong>S O P R E</strong></p>",
+        "title": "Épreuve II — Le clavier oublié",
+        "q": "<p>Un ancien appareil peut t'aider à déchiffrer cette suite de chiffres.</p><p><strong>777 33 7 666 7777</strong></p><p><em>Indice : avant les écrans tactiles, chaque touche cachait plusieurs lettres.</em></p>",
         "answers": [
           "repos"
         ],
@@ -577,7 +577,7 @@ const tickets = [
       {
         "type": "text",
         "title": "Épreuve IV — Le reflet de la nuit",
-        "q": "<p>Certains secrets ne se révèlent qu'à travers leur reflet.</p><p>De quoi parle cette phrase ?</p><div class='mirror-clue'>LES PLUS BEAUX NE SE RACONTENT PAS, ILS SE VIVENT LES YEUX FERMÉS</div>",
+        "q": "<p>Certains secrets ne se révèlent qu'à travers leur reflet.</p><p>Quel mot se cache dans ce message ?</p><div class='mirror-clue'>LA NUIT LAISSE PLACE AUX REVES</div>",
         "answers": [
           "reves",
           "rêves"
